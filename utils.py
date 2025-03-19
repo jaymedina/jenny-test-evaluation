ca056f6324c40e842a4e49c967a4af2e0102469e
Jenny V Medina <jenny.medina@sagebase.org>
2025-03-19 10:56:05 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -5,6 +5,10 @@ from glob import glob
 def extract_gs_file(folder):
     """Extract goldstandard file from folder."""
     files = glob(os.path.join(folder, "*"))
+
+    # Filter out the manifest file
+    files = [f for f in files if os.path.basename(f) != 'SYNAPSE_METADATA_MANIFEST.tsv']
+    
     if len(files) != 1:
         raise ValueError(
             "Expected exactly one goldstandard file in folder. "
